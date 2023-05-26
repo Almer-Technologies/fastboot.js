5c16a8b08a60c2c7f6b0c6422552803c25ca7c9c
mihaivo <mihai@almer-technologies.com>
2023-05-26 13:49:12 +0300

    Try fix

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ export { setDebugLevel, TimeoutError } from "./common";
 
 export { configure as configureZip } from "@zip.js/zip.js";
 
-export * from "./testJsdoc";
+// export * from "./testJsdoc";
 // @license-end
